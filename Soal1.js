1e0ceb5bf25095a21c7f0ed32d6d8f0dabc3a940
sleepy81k <nabilnico@gmail.com>
2020-09-18 22:01:43 +0700

    first

---
+++ Soal1.js
@@ -0,0 +1,8 @@
+
+const santriIkhwan  = ["sidik", "rahmad", "Syofyan", "Aldi", "Kardi"] 
+
+var prabu = [Lampung, Prabumulih,...Lain] = santriIkhwan
+
+console.log (Lampung)
+console.log (Prabumulih)
+console.log (Lain)
